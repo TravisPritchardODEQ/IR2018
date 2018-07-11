eb8c784617806b9c246c0d690fa21e3777fb3a25
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-07-11 15:45:26 -0700

    Trying to figure out parameter names
    Setting the parameters in the characteristic table to match the names in the wqstd_by_use_code table in the IR access database. Nee dto still match these with the parameter names in the resultsrawWATER table in the IR sql database.

--- Validation/anom_criteria_calc.R
+++ Validation/anom_criteria_calc.R
@@ -7,8 +7,16 @@ OWQI_data <- read.xlsx("Validation/OWQI_DATA_1980to2017.xlsx")
 
 #create anom_crit table
 anom_crit <- OWQI_data %>%
+  rename(Temperature = temp,
+         "Dissolved Oxygen" = d_o,
+         "Dissolved Oxygen Sat" = do_sat,
+         pH = ph,
+         Ammonia = nh3, 
+         "Phosphorus Elemental" = p,
+         "E. coli" = ecoli,
+         "Fecal Coliform" = fecal) %>%
   #make table long format 
-  gather(key = char, r,  temp:fecal) %>%
+  gather(key = char, r,  10:20) %>%
   #remove rows with now results
   filter(!is.na(r)) %>%
   group_by(HUC4, char) %>%
@@ -17,9 +25,8 @@ anom_crit <- OWQI_data %>%
             per1 = as.numeric((quantile(r, c(0.99, 0.01))["1%"]))) %>%
   #add information rows 
   mutate(AmbDatarange = "1980-2017",
-         StdRef = "OWQI") %>%
-  #change some variable names around
-  mutate(char = ifelse(char == "d_o", "do", ifelse(char == "do_sat", "dos", char )))
+         StdRef = "OWQI") 
+
 
 #save table to be loaded into R later
 save(anom_crit, file = "Validation/anom_crit.Rdata" )
