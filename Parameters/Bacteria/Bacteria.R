0d5c8fc706338b651261a3bc56c3f3d77ae465bf
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-07-30 10:58:00 -0700

    Bacteria: freshwater contact
    Adding freshwater contact analysis to Bacteria.R

--- Parameters/Bacteria/Bacteria.R
+++ Parameters/Bacteria/Bacteria.R
@@ -1,8 +1,10 @@
 source("Parameters/Bacteria/fun_Bacteria_data.R")
 source("Parameters/Bacteria/fun_coast_contact.R")
+source("Parameters/Bacteria/fun_fresh_contact.R")
 
 
 
 Results_censored <- Bacteria_data()
 
 Coast_contact_rec <- Coastal_Contact_rec()
+fresh_contact_rec <- Fresh_Contact_rec()
