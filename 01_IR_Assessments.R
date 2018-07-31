0ba2616f8c2f47dc1decc77921faf2b47ab15e54
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-07-31 08:54:10 -0700

    Initialize assessment script
    Initilize the script that pulls together all the assessment scripts. Designed to make running final assessment easier.

---
+++ 01_IR_Assessments.R
@@ -0,0 +1,10 @@
+# 2018 Oregon Integrated report assessments
+# Make sure IRlibrary is installed on maching
+# To install, run 00_library_install.R script.
+
+
+# Run Bacteria assesment
+source('Parameters/Bacteria/Bacteria.R')
+
+#Run pH assessment
+source('Parameters/pH/pH.R')
