084ba41fe1763e10832e2abd9499dc54fc1f12f7
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-07-31 14:26:29 -0700

    bacteria analysis: Add shellfish harvest script

--- Parameters/Bacteria/Bacteria.R
+++ Parameters/Bacteria/Bacteria.R
@@ -1,7 +1,7 @@
 source("Parameters/Bacteria/fun_Bacteria_data.R")
 source("Parameters/Bacteria/fun_coast_contact.R")
 source("Parameters/Bacteria/fun_fresh_contact.R")
-
+source("Parameters/Bacteria/fun_shell_harvest.R")
 
 # Bring data into analysis
 Results_censored_bacteria <- Bacteria_data("A:/Integrated_Report/IR_Database/IR_2018.accdb")
@@ -11,4 +11,7 @@ Bacteria_fresh_contact_rec <- Fresh_Contact_rec(Results_censored_bacteria)
 
 # Water Contact recreation - Coastal Water 
 Bacteria_Coast_contact_rec <- Coastal_Contact_rec(Results_censored_bacteria)
+
+#Shellfish Harvesting
+Bacteria_Shell_Harvest <- Shell_Harvest(Results_censored_bacteria)
 
