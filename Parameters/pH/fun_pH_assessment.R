83aef7f5d41da6c86ec3bfe3c13dd26eef53544b
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-08-07 12:07:38 -0700

    Adding binomial test to pH assessment

--- Parameters/pH/fun_pH_assessment.R
+++ Parameters/pH/fun_pH_assessment.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(IRlibrary)
 
 
 pH_assessment <- function(df) {
@@ -10,12 +11,14 @@ pH_assessment <- function(df) {
               pH_Min = min(pH_Min),
               pHMax = max(pH_Max),
               pH_code = first(pH_code)) %>%
-    mutate(Cat5 = ifelse(num_Samples > 5 & num_violation/num_Samples > 0.10 & num_violation >= 2, 1, 0),
+    mutate(k = excursions_conv(num_Samples),
+           Cat5 = ifelse(num_violation >= k , 1, 0),
            Cat3 = ifelse((num_Samples < 5 & num_violation < 2) | 
                            ((num_Samples >= 5 & num_Samples <= 9) & num_violation == 1),1,0),
            Cat3B = ifelse(num_Samples < 5 & num_violation >= 2, 1, 0),
-           Cat2 = ifelse((num_Samples >= 10 &  num_violation/num_Samples <= 0.10) | 
-                           ((num_Samples >= 5 & num_Samples <= 9) & num_violation == 0), 1, 0)
+           Cat2  = ifelse((num_Samples >= 10 &  num_violation < k) | 
+                           ((num_Samples >= 5 & num_Samples <= 9) & num_violation == 0), 1, 0),
+           sum = Cat5+Cat3+Cat3B+Cat2
            )
   return(pH_summary)
 }
