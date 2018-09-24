25606ae30237f07323bb73c8752c22216e7ad47d
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-09-24 14:29:59 -0700

    Update PH data script to include IR_validation function

--- Parameters/pH/fun_pH_data.R
+++ Parameters/pH/fun_pH_data.R
@@ -22,21 +22,38 @@ pH_data <- function(database) {
      "SELECT InputRaw.OrgID, InputRaw.MLocID, InputRaw.AU_ID, InputRaw.HUC4_Name, InputRaw.MonLocType, InputRaw.TribalLand, InputRaw.wqstd_code, InputRaw.BacteriaCo, InputRaw.Pollu_ID, InputRaw.ChrName, InputRaw.ActMediaName, InputRaw.ActMediaSubName, InputRaw.SampleFractName, InputRaw.ResStatusName, InputRaw.[Pollutant_DEQ WQS], InputRaw.Result, InputRaw.Result4IR, InputRaw.ResultOp4IR, InputRaw.ResultUnitName, InputRaw.ResultComment, InputRaw.ResultlabComment, InputRaw.ResultMeasQualDesc, Crit_pH.pH_Min, Crit_pH.pH_Max
 FROM Crit_pH INNER JOIN InputRaw ON Crit_pH.pH_code = InputRaw.pH_code
 WHERE (((InputRaw.wqstd_code)=10) AND ((InputRaw.ResStatusName)='Final'));
+
 ")
   
   
   odbcClose(IR.sql)
   
-  print(paste("Fetched", nrow(Results_import), "results from", length(unique(Results_import$STATION_KEY)), "monitoring locations" ))
+  print(paste("Fetched", nrow(Results_import), "results from", length(unique(Results_import$MLocID)), "monitoring locations" ))
   
   # Set factors to characters
   Results_import %>% map_if(is.factor, as.character) %>% as_data_frame -> Results_import
   
   
+
+# Data Validation ---------------------------------------------------------
+
+  
+  print("Validating Data")
+  
+  # Load validation table
+  load("Validation/anom_crit.Rdata")
+  
+  Results_valid <- IR_Validation(Results_import, anom_crit, "pH")
+  
+  rm(anom_crit)
+  
+
+# Censored data -----------------------------------------------------------
+
   print("Modify censored data")
   
   #run the censored data function to set censored data. This will use the lowest crit value from above
-  Results_censored <- Censored_data(Results_import, crit = `pH_Min` ) %>%
+  Results_censored <- Censored_data(Results_valid, crit = `pH_Min` ) %>%
     mutate(Result_cen = as.numeric(Result_cen))
   
   print(paste("Removing", sum(is.na(Results_censored$Result_cen)), "null values"))
