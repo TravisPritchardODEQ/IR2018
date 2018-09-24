b033893ee3deebbea865576be4359f5fb5d0c169
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-09-24 13:16:32 -0700

    Validation change
    Change validation criteria table to include all statewide data, and not broken by HUC4

--- Validation/anom_criteria_calc.R
+++ Validation/anom_criteria_calc.R
@@ -3,7 +3,7 @@ library(readxl)
 
 
 #read data into R
-OWQI_data <- read.xlsx("Validation/OWQI_DATA_1980to2017.xlsx")
+OWQI_data <- read_excel("Validation/OWQI_DATA_1980to2017.xlsx")
 
 #create anom_crit table
 anom_crit <- OWQI_data %>%
@@ -19,7 +19,7 @@ anom_crit <- OWQI_data %>%
   gather(key = char, r,  10:20) %>%
   #remove rows with now results
   filter(!is.na(r)) %>%
-  group_by(HUC4, char) %>%
+  group_by(char) %>%
   #create 99th and 1st percentile and summarize data
   summarise(per99 = as.numeric((quantile(r, c(0.99, 0.01))["99%"])),
             per1 = as.numeric((quantile(r, c(0.99, 0.01))["1%"]))) %>%
