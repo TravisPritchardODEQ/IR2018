06692fd3e65b1b74f4f24867fce58b902336b420
MERRICK Lesley <LMERRIC@deq.state.or.us>
2018-10-11 17:14:10 -0700

    working!

--- Parameters/BioCriteria/fun_bio_assessment.R
+++ Parameters/BioCriteria/fun_bio_assessment.R
@@ -2,11 +2,31 @@ biodata <- biodata %>%
   filter(Qualifier == "DQL=A") %>%
   filter(ID == "% Taxa Loss")
 
+#### Marine Western Coastal Forest
 MWCF_AU_sum = biodata %>%
-  filter(EcoRegion2 == "MARINE WEST COAST FOREST") %>%
-  group_by(AU_ID.y) %>%
-  summarise(n = "n()",
-            n_TLOver15P = sum(IndexTypeID >= 20),  ## Cat 5 n= 1 or more  
-            n_TL20to15P = sum(IndexTypeID > 15 & IndexTypeID < 20), # cat 5 if n is greater than or equal to 2, if n=1 cat 3B
-            n_TL14to9P = sum(IndexTypeID > 9 & IndexTypeID < 14), # cat 3D
-            N_TLless8P = sum(IndexTypeID <= 8)) # cat 2 
+  filter(Eco2 == "MWCF") %>%
+  group_by(AU_ID) %>%
+  summarise(num_Samples = n(),
+            n_over20PTL = sum(Score >= 20),
+            n_15to20PTL = sum(Score > 15 & Score < 20),
+            n_9to14PTL = sum(Score > 9 & Score < 14),
+            n_less8PTL = sum(Score <= 8)) %>%
+  mutate(IR_Cat = ifelse(num_Samples == 1 & n_over20PTL >=1 | num_Samples >= 2 & n_over20PTL >=1 | num_Samples >= 2 & n_15to20PTL >=1,"Cat5",
+                         ifelse(num_Samples == 1 & n_15to20PTL >=1,"Cat3B",
+                                ifelse(n_9to14PTL >= 1,"Cat3C",
+                                       ifelse(n_less8PTL >= 1,"Cat2","")))))
+
+
+#### Western Cordillera and Columbia Plateau 
+WC_AU_sum = biodata %>%
+  filter(Eco2 == "WC"|Eco2 == "COLD DESERTS") %>%
+  group_by(AU_ID) %>%
+  summarise(num_Samples = n(),
+            n_over20PTL = sum(ID >= 20),
+            n_15to20PTL = sum(ID > 15 & ID < 20),
+            n_9to14PTL = sum(ID > 9 & ID < 14),
+            n_less8PTL = sum(ID <= 8)) %>%
+  mutate(IR_Cat = ifelse(num_Samples == 1 & n_over20PTL >=1 | num_Samples >= 2 & n_over20PTL >=1 | num_Samples >= 2 & n_15to20PTL >=1,"Cat5",
+                         ifelse(num_Samples == 1 & n_15to20PTL >=1,"Cat3B",
+                                ifelse(n_15to20PTL >=1,"Cat3C",
+                                       ifelse(n_less8PTL >=1,"Cat2","")))))
