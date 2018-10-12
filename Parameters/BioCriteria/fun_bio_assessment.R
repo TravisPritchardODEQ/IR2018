01e15bbce7fd6e08cb9f78a9eca2e863d1353d18
MERRICK Lesley <LMERRIC@deq.state.or.us>
2018-10-11 17:14:46 -0700

    working again

--- Parameters/BioCriteria/fun_bio_assessment.R
+++ Parameters/BioCriteria/fun_bio_assessment.R
@@ -17,16 +17,3 @@ MWCF_AU_sum = biodata %>%
                                        ifelse(n_less8PTL >= 1,"Cat2","")))))
 
 
-#### Western Cordillera and Columbia Plateau 
-WC_AU_sum = biodata %>%
-  filter(Eco2 == "WC"|Eco2 == "COLD DESERTS") %>%
-  group_by(AU_ID) %>%
-  summarise(num_Samples = n(),
-            n_over20PTL = sum(ID >= 20),
-            n_15to20PTL = sum(ID > 15 & ID < 20),
-            n_9to14PTL = sum(ID > 9 & ID < 14),
-            n_less8PTL = sum(ID <= 8)) %>%
-  mutate(IR_Cat = ifelse(num_Samples == 1 & n_over20PTL >=1 | num_Samples >= 2 & n_over20PTL >=1 | num_Samples >= 2 & n_15to20PTL >=1,"Cat5",
-                         ifelse(num_Samples == 1 & n_15to20PTL >=1,"Cat3B",
-                                ifelse(n_15to20PTL >=1,"Cat3C",
-                                       ifelse(n_less8PTL >=1,"Cat2","")))))
