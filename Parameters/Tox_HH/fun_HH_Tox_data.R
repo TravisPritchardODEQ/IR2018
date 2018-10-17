09f90a16268770c4501e98fd43686e7a905ceece
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-10-16 17:35:00 -0700

    Modify to pull data from IR 2018 database, VW.ToxHH

--- Parameters/Tox_HH/fun_HH_Tox_data.R
+++ Parameters/Tox_HH/fun_HH_Tox_data.R
@@ -1,31 +1,24 @@
 require(rgdal)
 require(RODBC)
 require(tidyverse)
+require(IRlibrary)
 
 HH_tox_data <- function(database) {
   
   
 
   print("Fetch HH Tox data from IR database")
-  #connect to IR database view as a general user
-  # import bacteria data
-#   IR.sql <-  odbcConnectAccess2007("//deqlab1/Assessment/Integrated_Report/IR_Database/IR_2018_v2.accdb", case="nochange")
-#   
-#   
-#   
-#   # Get data from IR database where wqst = 16 and ResStatusName = Final
-#   # Join with Crit_ToxHH to get ToxHH Criteria
-#   Results_import <-
-#     sqlQuery(
-#       IR.sql,
-#       "SELECT InputRaw.OrgID, InputRaw.MLocID, InputRaw.AU_ID, InputRaw.HUC4_Name, InputRaw.MonLocType, InputRaw.TribalLand, InputRaw.wqstd_code, InputRaw.Pollu_ID, InputRaw.ChrName, InputRaw.ActMediaName, InputRaw.ActMediaSubName, InputRaw.SampleFractName, InputRaw.ResStatusName, InputRaw.[Pollutant_DEQ WQS], InputRaw.Result, InputRaw.Result4IR, InputRaw.ResultOp4IR, InputRaw.ResultUnitName, InputRaw.ResultComment, InputRaw.ResultMeasQualDesc, Crit_ToxHH.WaterOrganism, Crit_ToxHH.Organism, Crit_ToxHH.Organism_SW
-# FROM InputRaw INNER JOIN Crit_ToxHH ON InputRaw.Pollu_ID = Crit_ToxHH.Pollu_ID
-#       WHERE (((InputRaw.wqstd_code)=16) AND ((InputRaw.ResStatusName)='Final'));
-#       ")
-#   
-#   
-#   odbcClose(IR.sql)
-#   
+# connect to IR database view as a general user
+# import bacteria data
+  IR.sql <-   odbcConnect(database)
+  
+  
+  # Get data from IR database where wqstd_code = 12, ResStatusName = Final, 
+  # Join with Crit_Temp to get temperature Criteria and spawn ?
+  Results_import <-    sqlFetch(IR.sql, "dbo.VW_ToxHH") 
+  
+  
+  odbcClose(IR.sql)
 #   
   #######################################################
   ###         Temporary save of Results import -      ###
@@ -34,9 +27,9 @@ HH_tox_data <- function(database) {
   
   #save(Results_import, file = "Parameters/Tox_HH/Results.RData")
   
-  #Load in saved dataset
-  load("Parameters/Tox_HH/Results.RData")
-  
+  # #Load in saved dataset
+  # load("Parameters/Tox_HH/Results.RData")
+  # 
   print(paste("Fetched", nrow(Results_import), "results from", length(unique(Results_import$MLocID)), "monitoring locations" ))
   
   
@@ -50,7 +43,7 @@ HH_tox_data <- function(database) {
   # Get all the standards to be used when dealing with the censored data
   Results_crit <- Results_import %>%
     # Get lowest criteria value to set censored results
-    mutate(lowest_crit = pmin(SS_Crit, Geomean_Crit, Perc_Crit, na.rm = TRUE))
+    mutate(lowest_crit = pmin(WaterOrganism, Organism, Organism_SW, na.rm = TRUE))
   
   
   print("Modify censored data")
