2003fdc807b3b8fde017e8d3ce9f54225b020b0c
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-10-29 09:35:07 -0700

    chl-a data function

---
+++ Parameters/chl_a/fun_chla_Data.R
@@ -0,0 +1,62 @@
+require(rgdal)
+require(RODBC)
+library(tidyverse)
+library(IRlibrary)
+
+#Still need to figure out censored data piece
+
+
+Temp_data <- function(database) {
+  print("Fetch Chl data from IR database")
+  #connect to IR database view as a general user
+  # import Temperature data
+  IR.sql <-   odbcConnect(database)
+  
+  
+  # Get data from IR database where wqstd_code = 12, ResStatusName = Final, 
+  # Join with Crit_Temp to get temperature Criteria and spawn ?
+  Results_import <-    sqlFetch(IR.sql, "dbo.VW_Chl") 
+  
+  
+  odbcClose(IR.sql)
+  
+  
+  
+  print(paste("Fetched", nrow(Results_import), "results from", length(unique(Results_import$MLocID)), "monitoring locations" ))
+  
+  # Set factors to characters
+  Results_import %>% map_if(is.factor, as.character) %>% as_data_frame -> Results_import
+  
+  
+  
+  # Data validation ---------------------------------------------------------
+  
+  print("Validating Data")
+  
+  # Load validation table
+  load("Validation/anom_crit.Rdata")
+  
+  Results_valid <- IR_Validation(Results_import, anom_crit, "Chl")
+  
+  
+  
+  # Censored data ------------------------------------------------------------
+  
+  
+  
+  print("Modify censored data")
+  
+  #run the censored data function to set censored data. This will use the lowest crit value from above 
+  Results_censored <- Censored_data(Results_valid, crit = `Chla_Criteria` ) %>%
+    mutate(Result_cen = as.numeric(Result_cen))
+  
+  print(paste("Removing", sum(is.na(Results_censored$Result_cen)), "null values"))
+  
+  Results_censored <- Results_censored%>%
+    filter(!is.na(Result_cen))
+  
+  print("Data fetch and censored data modifications complete")
+  
+  
+  return(Results_censored)
+}
