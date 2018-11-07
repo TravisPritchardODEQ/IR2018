8becbd6eadde680f28bc5d5d14f44706c5859436
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-11-07 13:00:24 -0800

    IRlibrary duplicate check

---
+++ IRlibrary/R/dup_check.R
@@ -0,0 +1,21 @@
+#' dup_check
+#' 
+#' This function perfroms a duplicate check based on 
+#' MLocID, ActStartD, ActStartT, ChrName and Results
+#' 
+#' @param chr parameter name used for output filename only
+#' @param df dataframe for dup check. This should be the dataframe imported from IR database
+#' @export
+#' @examples  
+
+
+dup_check <- function(df, chr) {
+  
+  
+  Dup_data <- df %>%
+    group_by(MLocID, ActStartD, ActStartT, ChrName, Result) %>%
+    summarise(count = n()) %>%
+    filter(count > 1)
+  
+  write.csv(Dup_data, paste0("/Parameters/", chr, "dup_data.csv"))
+}
