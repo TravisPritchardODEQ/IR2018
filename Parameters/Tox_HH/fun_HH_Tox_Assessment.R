7ccb3b27faddcea40ddaa52968c60d163d71654e
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-11-14 12:19:45 -0800

    Adding Sample fraction

--- Parameters/Tox_HH/fun_HH_Tox_Assessment.R
+++ Parameters/Tox_HH/fun_HH_Tox_Assessment.R
@@ -11,20 +11,24 @@ tox_HH_assesment <- Results_censored_tox_HH %>%
 #Write table here
   
 tox_HH_categories <- tox_HH_assesment %>%
-  group_by(AU_ID, Pollutant) %>%
+  group_by(AU_ID, Pollutant, SampleFractName,Crit_Fraction) %>%
   summarise(crit = max(crit),
             num_samples = n(),
             num_violation = sum(violation),
             geomean = geo_mean(Result_cen)) %>%
-  mutate(critical_num = excursions_conv(num_samples),
-         IR_category = ifelse(num_violation >= critical_num, "Cat5", 
-                              ifelse(num_samples >= 3 & geomean >= crit, "Cat5", 
+  ungroup() %>%
+  group_by(AU_ID, Pollutant) %>%
+  mutate(num_fraction_types =  n(),
+         critical_num = excursions_conv(num_samples),
+         IR_category = ifelse(num_violation >= critical_num, "Cat5 (Binomial)", 
+                              ifelse(num_samples >= 3 & geomean >= crit, "Cat5 (Geomean)", 
                                      ifelse(num_samples < 3 & num_violation >= 1, "Cat3B", 
-                                            ifelse(num_samples < 3 & num_violation == 3, "Cat3", "Cat2" )))))
+                                            ifelse(num_samples < 3 & num_violation == 0, "Cat3", "Cat2" ))))) %>%
+  arrange(AU_ID, Pollutant)
  
 #write tablehere
 
 
 # To do - 
 #Figure out fractions piece
-# test
+# make sure units are correct in data pull
