2345323415e79ea7ad2ffb8b8e2566ba2dbb5254
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-11-14 09:38:50 -0800

    Toxics HH - 3 consecutive year method

--- Parameters/Tox_HH/fun_HH_Tox_Assessment.R
+++ Parameters/Tox_HH/fun_HH_Tox_Assessment.R
@@ -1,4 +1,4 @@
-
+library(lubridate)
 
 #fun_Tox_HH_analysis <- function(df){} 
 
@@ -6,14 +6,40 @@
 tox_HH_assesment <- Results_censored_tox_HH %>%
   mutate(violation = ifelse(Result_cen > crit, 1, 0 )) 
 
-#Write table here
 
+#Figure out where we have 3 years of consecutive data 
+  # add column for year
+  # Pare down datatable to only AU, charcateristic, and year
+  # Group by and summarze to get unique values for AU, chr. and year
+  # Regoup by AU and CHY
+  # Order by AU, Chr, and year
+  # Calculate difference in years between the year from the grouped year above
+    # This mainitaings groups, i.e. we will not be comparing different chrs or AUs
+  # If the diff value for a row is 1, and the row above is also 1, then that is a consecutive three year period
+consecutive_yrs <- tox_HH_assesment %>%
+  mutate(year = year(ActStartD)) %>%
+  select(AU_ID, ChrName, year) %>%
+  group_by(AU_ID, ChrName, year) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  group_by(AU_ID, ChrName) %>%
+  arrange(AU_ID, ChrName, year) %>%
+  mutate(diffs = c(0, diff(year))) %>%
+  mutate(consecutive_3yr = ifelse((lag(diffs , 1) == 1)  & diffs == 1, 1, 0 ) ) 
+
+#Write table here
+  
 tox_HH_categories <- tox_HH_assesment %>%
+  left_join(select(consecutive_yrs, AU_ID, ChrName, consecutive_3yr), by = c("AU_ID", "ChrName")) %>%
   group_by(AU_ID, Pollutant) %>%
-  summarise(num_samples = n(),
+  summarise(consecutive_3yr = max(consecutive_3yr),
+            num_samples = n(),
             num_violation = sum(violation)) %>%
-  mutate(critical_num = excursions_conv(num_samples)) %>%
-  mutate(IR_category = ifelse(num_violation >= critical_num, "Cat5", "Cat2" ))
+  mutate(critical_num = excursions_conv(num_samples),
+         IR_category = ifelse(consecutive_3yr == 1 & num_violation >= critical_num, "Cat5", 
+                              ifelse(consecutive_3yr == 0 & num_violation >= 2, "Cat5", 
+                                     ifelse(consecutive_3yr == 0 & num_violation == 1, "Cat3B", "ERROR" ))))
+ 
 
 
 
