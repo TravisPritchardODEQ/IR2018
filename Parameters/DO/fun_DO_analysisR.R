15afa980a3b3816f0d8e24190178a18e50362f64
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-11-26 09:49:45 -0800

    Adding year round Do categorization

--- Parameters/DO/fun_DO_analysisR.R
+++ Parameters/DO/fun_DO_analysisR.R
@@ -1,7 +1,9 @@
 library(lubridate)
 
 
-## year round
+
+# Year round --------------------------------------------------------------
+
 
 # add spawn start and end dates as dates, include indicator if actdate is within spawn
 # add critical period start and end dates, include indicator is actdate is within critperiod
@@ -26,6 +28,42 @@ results_cont_summary <- Results_spawndates %>%
          !is.na(AU_ID))
 
 
-# data to be analyzed using contiuous criteria
+# initial contiuous criteria analysis
+# filter down to AUs that are to be evaluated with cont metrics
+# Filter down to only 30-D, 7-Mi, and daily minimums
+# Flag various violations
 continuous_data_analysis <- Results_spawndates %>%
-  filter(AU_ID %in% results_cont_summary$AU_ID)
+  filter(AU_ID %in% results_cont_summary$AU_ID) %>%
+  filter(ResultBasesName %in% c("30DADMean", "7DADMin", "Minimum")) %>%
+  mutate(Violation = ifelse(ResultBasesName == "30DADMean" & Result4IR < crit_30D, 1, 
+                            ifelse(ResultBasesName == "7DADMin" & Result4IR < crit_7Mi, 1, 
+                                   ifelse(ResultBasesName == "Minimum" & Result4IR < crit_Min, 1, 0 )))) 
+
+continuous_data_categories <- continuous_data_analysis %>%
+  group_by(AU_ID, DO_Class) %>%
+  summarise(Total_violations = sum(Violation),
+            Sum_30D_violations = sum(Violation [ResultBasesName == "30DADMean"]),
+            Sum_7mi_violations = sum(Violation [ResultBasesName == "7DADMin"]),
+            Sum_abs_min_violations = sum(Violation [ResultBasesName == "Minimum"])) %>%
+  mutate(category = ifelse(DO_Class != "Cold Water" & 
+                             (Sum_30D_violations >= 2 |
+                                Sum_7mi_violations >= 2 |
+                                Sum_abs_min_violations >= 2), "Cat 5", 
+                           ifelse(DO_Class == "Cold Water" & 
+                                    (Sum_7mi_violations >= 2 |
+                                       Sum_abs_min_violations >= 2), "Cat 5", 
+                                 ifelse(DO_Class == "Cold Water" &
+                                           Sum_30D_violations >= 2 &
+                                           Sum_7mi_violations < 2 &
+                                           Sum_abs_min_violations < 2, "Check percent Sat",  
+                                         ifelse(Sum_30D_violations < 2 &
+                                                  Sum_7mi_violations < 2 &
+                                                  Sum_abs_min_violations < 2, "Cat 2", "Error" )))))
+
+
+# Data to be used to check percent saturation
+perc_sat_check <- continuous_data_analysis %>%
+  filter(AU_ID %in% unique(subset(continuous_data_categories, category == "Check percent Sat" )$AU_ID) )
+  
+  
+  
