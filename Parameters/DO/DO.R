e5a83575d9ec799f6a8a66d85e05e4a84c21597b
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-11-27 15:13:37 -0800

    writing to DO.R

--- Parameters/DO/DO.R
+++ Parameters/DO/DO.R
@@ -1,13 +1,20 @@
 source("Parameters/DO/fun_DO_data.R")
-#source("Parameters/Temperature/fun_temp_analysis.R")
+source("Parameters/DO/fun_DO_yearround.R")
 source("Parameters/DO/fun_DO_spawn.R")
 
 
 
 Results_censored_DO <- DO_data("IR 2018")
 
-Spawning_analysis_list <- DO_spawning_analysis(Results_censored_DO)
 
+Yearround_analysis_list <-  DO_year_round_analysis(Results_censored_DO)
+# Because we can't get two values from function, they are put in list
+# and need to be extracted
+DO_yearround_continuous <- Yearround_analysis_list[[1]]
+DO_yearround_instantaneous <- Yearround_analysis_list[[2]]
+
+
+Spawning_analysis_list <- DO_spawning_analysis(Results_censored_DO)
 
 # Because we can't get two values from function, they are put in list
 # and need to be extracted
