cfcdca09550012a928aae7d8c7545c3f024be3e3
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-12-03 16:24:02 -0800

    Adding data validation to DO data analysis

--- Parameters/DO/fun_DO_data.R
+++ Parameters/DO/fun_DO_data.R
@@ -31,16 +31,16 @@ DO_data <- function(database) {
   
   
   # Data validation ---------------------------------------------------------
-  Results_valid <-Results_import
+
   
-  # print("Validating Data")
-  # 
-  # # Load validation table
-  # load("Validation/anom_crit.Rdata")
-  # 
-  # Results_valid <- IR_Validation(Results_import, anom_crit, "DO")
-  # 
-  
+  print("Validating Data")
+
+  # Load validation table
+  load("Validation/anom_crit.Rdata")
+
+  Results_valid <- IR_Validation(Results_import, anom_crit, "DO")
+
+
   
   # Censored data ------------------------------------------------------------
   
