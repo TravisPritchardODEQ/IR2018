890ce8fa8acd0dd7564325b73ab5d9b1ee64c7f5
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-12-13 16:23:42 -0800

    Modifying bacteria data to include data validation

--- Parameters/Bacteria/Bacteria.R
+++ Parameters/Bacteria/Bacteria.R
@@ -1,10 +1,30 @@
 source("Parameters/Bacteria/fun_Bacteria_data.R")
+source("Parameters/Bacteria/fun_Bacteria_data_censored.R")
 source("Parameters/Bacteria/fun_coast_contact.R")
 source("Parameters/Bacteria/fun_fresh_contact.R")
 source("Parameters/Bacteria/fun_shell_harvest.R")
 
+
+# Data Prep ---------------------------------------------------------------
+
+
 # Bring data into analysis
-Results_censored_bacteria <- Bacteria_data("IR 2018")
+Bacteria_results <- Bacteria_data("IR 2018")
+
+#######################################################################################################
+###                         Stop here and review the invalid data file.                             ###
+###                      For valid data, mark the Conclusion field as Valid                         ###
+#######################################################################################################
+
+#Reinput the data after the manual data validation step
+Validated_reults <- IR_Validation_Import(file = "Parameters/Invalid_data/Invalid-Bacteria.xlsx", df = Bacteria_results)
+
+# Perfrom the data censoring and cleaning
+Results_censored_bacteria <- Bacteria_data_censored(Validated_reults)
+
+
+# Data analysis -----------------------------------------------------------
+
 
 # Water Contact Recreation - Freshwater
 Bacteria_fresh_contact_rec <- Fresh_Contact_rec(Results_censored_bacteria)
--- Parameters/Bacteria/fun_Bacteria_data.R
+++ Parameters/Bacteria/fun_Bacteria_data.R
@@ -38,26 +38,6 @@ require(IRlibrary)
   
   rm(anom_crit)
   
-  # Get all the standards to be used when dealing with the censored data
-  Results_crit <- Results_valid %>%
-    # Get lowest criteria value to set censored results
-    mutate(lowest_crit = pmin(SS_Crit, Geomean_Crit, Perc_Crit, na.rm = TRUE))
-
-    
-  
-  print("Modify censored data")
-  
-  #run the censored data function to set censored data. This will use the lowest crit value from above
-  Results_censored <- Censored_data(Results_crit, crit = `lowest_crit` ) %>%
-    mutate(Result_cen = as.numeric(Result_cen))
-  
-  print(paste("Removing", sum(is.na(Results_censored$Result_cen)), "null values"))
-  
-  Results_censored <- Results_censored %>%
-    filter(!is.na(Result_cen))
-  
-  print("Data fetch and censored data modifications complete")
-
-return(Results_censored)
+  return(Results_valid)
   
 }
