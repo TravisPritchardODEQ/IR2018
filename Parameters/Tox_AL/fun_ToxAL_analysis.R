15efbf1f10393963d9d27046306587fbf21f9d88
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2018-12-26 14:30:18 -0800

    Modifying endosulfan to use new summed parameter non-detect summing
    If summing parameters, use 0 for non-detects

--- Parameters/Tox_AL/fun_ToxAL_analysis.R
+++ Parameters/Tox_AL/fun_ToxAL_analysis.R
@@ -6,7 +6,8 @@ endosulfan_data <- Results_censored %>%
   # Filter for endosulfan Pollu_IDs
   filter(Pollu_ID %in% c(77,78,79 )) %>%
   # Set a flag for if the value is total_endosulfan
-  mutate(is_total_endosulfan = ifelse(Pollu_ID == 77, 1, 0 ) ) %>%
+  mutate(is_total_endosulfan = ifelse(Pollu_ID == 77, 1, 0 ),
+         summed_censored_value = ifelse(Result_Operator == "<", 0, Result_cen)) %>%
   # Set a group that identifies a single sample
   group_by(OrganizationID, MLocID, SampleStartDate,SampleStartTime, Analytical_method, act_depth_height) %>%
   # Flag if the group has a total endosulfan result 
@@ -21,21 +22,22 @@ endosulfan_data <- Results_censored %>%
   # Count the number of endosulfan types in the group
   # this is needed to be sure that when we are summing the inidividual componants
   # we have both of them
-  mutate(num_types = n_distinct(Char_Name)) %>%
+  mutate(num_types = n_distinct(Char_Name),
+         summed_percent_nondetect = round(sum(Result_Operator == "<")/n()*100)) %>%
   #Filter out the results where the group has only alpha or beta, but not both
   filter(Pollu_ID == 77 | num_types == 2 ) %>%
   # Create a comment field so we know where we did the summing
   # Also add the values in the group to get the summed value
   # All values in the group willget this value
   mutate(IR_note = ifelse(Pollu_ID %in% c(78,79), "Sum of alpha and beta endosulfan", "" ),
-         Summed_values = sum(Result_cen)) %>%
+         Summed_values = sum(summed_censored_value)) %>%
   # Keep only the first row. This preserves all the metadata
   filter(row_number() == 1) %>%
   # Change the Char_Name to Endosulfan and the Result_cen column to the summed value
   mutate(Char_Name = "Endosulfan",
          Result_cen = Summed_values) %>%
   # get rid of extra columns that were created
-  select(-Summed_values,  -num_types,  -Has_total_endosulfan, -is_total_endosulfan)
+  select(-Summed_values,  -num_types,  -Has_total_endosulfan, -is_total_endosulfan, -summed_censored_value)
 
 
 results_analysis <- Results_censored %>%
@@ -61,7 +63,7 @@ Results_tox_AL_analysis <- results_analysis %>%
   # If group has Total fractionin it, mark with a 1. If ony dissolved, mark with 0
   mutate(Has_Crit_Fraction = ifelse(Fraction == "Total" & max(Simplified_sample_fraction) == "Total", 1, 
                                     ifelse(Fraction == "Dissolved" & max(Simplified_sample_fraction) != "Total", 1, 0 ))) %>%
-  # Filter out the results that do not macth criteira fraction, if the group has matching criteria. Also keep where whole group does not march
+  # Filter out the results that do not macth criteira fraction, if the group has matching criteria. Also keep where whole group does not match
   ungroup() %>%
   filter((Has_Crit_Fraction == 1 & Simplified_sample_fraction == Fraction) | Has_Crit_Fraction == 0) %>%
   # evaluate against the "most stringent of the acute or chronic criterion" - methodology
@@ -85,6 +87,7 @@ Results_tox_AL_categories <- Results_tox_AL_analysis %>%
   summarise(criteria_fraction = first(Fraction),
             num_samples = n(),
             percent_3d = round(sum(Result_Operator == "<" & IRResultNWQSunit > evaluation_crit )/num_samples * 100),
+            summed_percent_nondetect = sum(summed_percent_nondetect)/n(),
             num_fraction_types = n_distinct(Simplified_sample_fraction),
             num_samples_total_fraction = sum(Simplified_sample_fraction == "Total"),
             num_Samples_dissolved_fraction = sum(Simplified_sample_fraction == "Dissolved"),
@@ -96,10 +99,10 @@ Results_tox_AL_categories <- Results_tox_AL_analysis %>%
                                                             num_Samples_dissolved_fraction + (num_samples_total_fraction - num_excursions_total_fraction ) )), 
             critical_excursions = excursions_tox(num_samples_crit_excursion_calc)) %>%
   # Assign categories
-  mutate(IR_category = ifelse(num_samples_crit_excursion_calc == 1 & num_excursions_all == 1, "Cat 3B",
-                              ifelse(num_samples_crit_excursion_calc == 1 & num_excursions_all == 0, "Cat 3",
+  mutate(IR_category = ifelse(percent_3d == 100 | (summed_percent_nondetect == 100 & !is.na(summed_percent_nondetect)), "Cat 3D",
+                              ifelse(num_samples_crit_excursion_calc == 1 & num_excursions_all == 1, "Cat 3B",
                                      ifelse((Char_Name == "Alkalinity, total" | Char_Name == "Alkalinity, bicarbonate") & num_excursions_all > 0, "Cat 3B", 
-                                            ifelse(percent_3d == 100, "Cat 3D",  
+                                            ifelse(num_samples_crit_excursion_calc == 1 & num_excursions_all == 0, "Cat 3", 
                                                    ifelse(num_excursions_all > critical_excursions, "Cat 5", 
                                                           ifelse(num_excursions_all <= critical_excursions, "Cat 2", "ERROR" )))) ) ))
                                     
@@ -111,3 +114,4 @@ Results_tox_AL_categories <- Results_tox_AL_analysis %>%
 #TO do
 # Summed constiuents and data censoring. How do we sum up values below detection limit?
 # Chlordane
+    # Need to update list from Lori
