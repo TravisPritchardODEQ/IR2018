308d1f6cb47ac99c959d81966317b41ea0460ece
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2019-01-28 12:25:23 -0800

    Better IRlibrary install code

--- 00_library_install.R
+++ 00_library_install.R
@@ -1,9 +1,16 @@
 # This script will install the IRlibrary library. It should be run failrly often, as i am still
 # adding functions 
 
-install.packages("devtools")
+
+# Check to see if devtools is installed. If not, install it
+devtools_pkg <- "devtools"
+
+new.packages <- devtools_pkg[!(devtools_pkg %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Load devtools and install library
 library("devtools")
 
-setwd('./IR2018')
-install("IRlibrary")
+install("IRlibrary", upgrade = 'always')
+
 
