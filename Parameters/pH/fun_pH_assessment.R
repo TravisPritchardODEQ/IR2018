1b7243b371e1a7c7b839abec290ff59b757b9fab
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2019-05-17 15:54:46 -0700

    pH - correct cat 3 assignments
    Cat 3 assignments were not working right. Now they are.

--- Parameters/pH/fun_pH_assessment.R
+++ Parameters/pH/fun_pH_assessment.R
@@ -31,13 +31,14 @@ IR_export(pH_summary, "Parameters/pH/Data_Review", "pH", "data" )
               pH_high_crit = max(pH_Max),
               pH_code = first(pH_code)) %>%
     mutate(critical_excursions = excursions_conv(num_Samples),
-           IR_category = ifelse(num_violation >= critical_excursions, 'Cat5', 
-                                ifelse((num_Samples < 5 & num_violation < 2), 'Cat3', 
-                                       #| ((num_Samples >= 5 & num_Samples <= 9) & num_violation == 1), 'Cat3', 
-                                       ifelse(num_Samples < 5 & num_violation >= 1, 'Cat3B', 
-                                              ifelse((num_Samples >= 5 &  num_violation < critical_excursions), 'Cat2', 
-                                                     #| ((num_Samples >= 5 & num_Samples <= 9) & num_violation == 0), 'Cat2', 
-                                                     "ERROR")))))
+           IR_category =  case_when(num_Samples < 5 & num_violation >= 2 ~ 'Cat3B',
+                                    num_Samples < 5 & num_violation < 2 ~ 'Cat3',
+                                    num_Samples >= 5 & num_violation >= critical_excursions ~ 'Cat5',
+                                    num_Samples >= 5 & num_violation < critical_excursions ~ 'Cat2',
+                                    TRUE ~ 'ERROR')) 
+           
+           
+         
            
   return(pH_categories)
 }
