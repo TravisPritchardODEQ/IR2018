c623d8025eac6b146053a9aff90608e02a71ad52
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2019-12-02 08:38:29 -0800

    Rollup fixes
    Fixing various errors in rollup file

--- Parameters/DO/fun_DO_yearround.R
+++ Parameters/DO/fun_DO_yearround.R
@@ -1,5 +1,5 @@
-
 DO_year_round_analysis <- function(df){
+
   
   library(lubridate)
   library(odbc)
@@ -274,7 +274,7 @@ yr_round_cont_DO_data_analysis <- yr_round_cont_DO_data_analysis %>%
 yr_round_cont_DO_data_analysis <- DO_Dup_remover(yr_round_cont_DO_data_analysis, 
                                                  filename = "Parameters/DO/DO_YearRound_continuous_Duplicated.csv")
 
-IR_export(yr_round_cont_DO_data_analysis, "Parameters/DO/Data_Review", "DO_YearRound_continuous", "data" )
+IR_export(yr_round_cont_DO_data_analysis, "Parameters/DO/Data_Review", "DO_YearRound_continuous", "data" )df
 
 
 
