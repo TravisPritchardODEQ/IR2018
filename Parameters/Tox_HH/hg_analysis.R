43beec3f76cd68897b477572a5784b8c8eaf2c8d
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2020-03-02 10:29:31 -0800

    HG tissue
    Handle composots better when they are labeled in Activity type as opposed to sampling method

--- Parameters/Tox_HH/hg_analysis.R
+++ Parameters/Tox_HH/hg_analysis.R
@@ -8,20 +8,21 @@ fun_Tox_HH_tissue_hg_analysis <- function(df){
 
   
   categories <- analysis %>%
-    group_by(AU_ID, Char_Name,SamplingMethod) %>%
+    mutate(is_composite = case_when(grepl("composite", SamplingMethod, ignore.case = TRUE) ~ 1,
+                                    grepl("composite", Activity_Type, ignore.case = TRUE) ~ 1,
+                                    TRUE ~ 0)) %>%
+    group_by(AU_ID, Char_Name,is_composite) %>%
     summarise(OWRD_Basin = first(OWRD_Basin),
               crit = max(crit),
               num_samples = n(),
-              evaluation_mean = case_when(max(SamplingMethod != "Composite") & n() >= 3 ~ geo_mean(Result_cen),
-                                          max(SamplingMethod == "Composite")& n() >= 2 ~ mean(Result_cen)),
+              evaluation_mean = case_when(max(is_composite != 1) & n() >= 3 ~ geo_mean(Result_cen),
+                                          max(is_composite == 1) & n() >= 2 ~ mean(Result_cen)),
               analysis_comment = paste0(Result_UID, collapse = ", ")
                             ) %>%
-    mutate(analysis_comment = case_when(SamplingMethod != "Composite" ~ paste("Result is the geometric mean of result_UIDs:",
-                                                                         analysis_comment, 
-                                                                         " - due to multiple results at same date"), 
-                                        SamplingMethod == "Composite" ~ paste("Result is the arithmetic mean of result_UIDs:",
-                                                                              analysis_comment, 
-                                                                              " - due to multiple results at same date"),
+    mutate(analysis_comment = case_when(is_composite != 1 ~ paste("Result is the geometric mean of result_UIDs:",
+                                                                         analysis_comment), 
+                                        is_composite == 1 ~ paste("Result is the arithmetic mean of result_UIDs:",
+                                                                              analysis_comment),
                                         TRUE ~ "")) %>%
     ungroup() %>%
     mutate(IR_category = case_when(evaluation_mean > crit ~ 1,
