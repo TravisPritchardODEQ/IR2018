273f176bf579b29401b6f3347cdc561bebf3cbc5
MERRICK Lesley <LMERRIC@deq.state.or.us>
2020-03-27 13:44:27 -0700

    building attains assessment upload files

--- ATTAINS/ATTAINS_UPLOAD/AU_spawn_dates.R
+++ ATTAINS/ATTAINS_UPLOAD/AU_spawn_dates.R
@@ -18,6 +18,21 @@ FROM     InputRaw INNER JOIN
 
 AU_Temp_Spawn <- DBI::dbGetQuery(con, Query)
 
+AU_Temp_Spawn <-  AU_Temp_Spawn %>%
+mutate(start_rank = case_when(Temp_SpawnStart == '8/1'~ 1,
+                              Temp_SpawnStart == "8/15" ~ 2,
+                              Temp_SpawnStart == '9/1' ~ 3,
+                              Temp_SpawnStart == '9/15' ~ 4,
+                              Temp_SpawnStart == '10/1' ~ 5,
+                              Temp_SpawnStart == '10/15' ~ 6,
+                              Temp_SpawnStart == '1/1' ~ 7,
+                              Temp_SpawnStart == '4/1'~ 8,
+                              Temp_SpawnStart == '11/1'~ 9)) %>%
+  mutate(end_rank = case_when(Temp_SpawnEnd == '3/31' ~ 1,
+                              Temp_SpawnEnd == '5/15' ~ 2,
+                              Temp_SpawnEnd == '6/15' ~ 3,
+                              Temp_SpawnEnd == '7/15' ~ 4))
+
 Query <- glue::glue_sql("SELECT DISTINCT InputRaw.AU_ID, 
 InputRaw.SpawnCode as DO_SpawnCode, 
 LU_Spawn.SpawnStart as DO_SpawnStart, 
@@ -26,7 +41,21 @@ LU_Spawn.Spawn_dates as DO_Spawn_dates
 FROM     InputRaw INNER JOIN
                   LU_Spawn ON InputRaw.DO_SpawnCode = LU_Spawn.SpawnCode", .con = con)
 
-AU_DO_Spawn <- DBI::dbGetQuery(con, Query)
-
+AU_DO_Spawn <- DBI::dbGetQuery(con, Query)  
+              
+AU_DO_Spawn <-  AU_DO_Spawn %>%
+                mutate(start_rank = case_when(DO_SpawnStart == '8/1'~ 1,
+                                 DO_SpawnStart == "8/15" ~ 2,
+                                 DO_SpawnStart == '9/1' ~ 3,
+                                 DO_SpawnStart == '9/15' ~ 4,
+                                 DO_SpawnStart == '10/1' ~ 5,
+                                 DO_SpawnStart == '10/15' ~ 6,
+                                 DO_SpawnStart == '1/1' ~ 7,
+                                 DO_SpawnStart == '4/1'~ 8,
+                                 DO_SpawnStart == '11/1'~ 9)) %>%
+                mutate(end_rank = case_when(DO_SpawnEnd == '3/31' ~ 1,
+                                            DO_SpawnEnd == '5/15' ~ 2,
+                                            DO_SpawnEnd == '6/15' ~ 3,
+                                            DO_SpawnEnd == '7/15' ~ 4))
 
 
--- ATTAINS/ATTAINS_UPLOAD/Assessment_Upload.R
+++ ATTAINS/ATTAINS_UPLOAD/Assessment_Upload.R
@@ -5,13 +5,15 @@ library(tidyverse)
 library(IRlibrary)
 library(data.table)
 require(RODBC)
+library(readxl)
 
 #This script will generate the upload for the assessment portion of ATTAINS 
 
 #this is test dataset filtered from \\deqhq1\WQASSESSMENT\2018IRFiles\2018_WQAssessment\Draft List\Rollup\Basin_categories 
 # once finalized, this should be a table in the IR2018 database 
-rollup <- read.csv("ATTAINS/ATTAINS_UPLOAD/Test_Rollup_Parameters.csv") %>%
-          filter(AU_ID == 'OR_SR_1707030110_05_102624') ## remove this
+rollup <- read.csv("ATTAINS/ATTAINS_UPLOAD/ALL BASINS_Parameters.csv") %>%
+          #filter(AU_ID == 'OR_LK_1705011006_05_100541')
+          filter(OWRD_Basin == 'Owyhee' | OWRD_Basin == 'Umpqua') ## remove this
 
 # connect to IRdatabase  
 IR.sql <-   odbcConnect("IR 2018")
@@ -20,14 +22,17 @@ LU_spawn <- sqlFetch(IR.sql, "dbo.LU_Spawn")
 BU <- sqlFetch(IR.sql, "dbo.LU_BenUseCode") 
 ## make DB table? 
 BU_2_Pollu <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Rollup/LU Bus.csv")
-AU_tbl <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Rollup/AU_names.csv")
-
+AU_tbl <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Rollup/AU_names.csv")  ## update with columbia slough split
+LU_delist <- read_excel("ATTAINS/ATTAINS_UPLOAD/Delisting_reasons_attains.xlsx", sheet = "Sheet1")
+delistings <- read.csv("ATTAINS/ATTAINS_UPLOAD/ALL BASINS_delistingsv8.csv") %>% 
+              left_join(LU_delist, by= c('Reason_Code'='reason_code')) %>% 
+              filter(!is.na(Reason_Code))
 #### Build Assessment Parameter table #### 
 # need to add Delisting stuff
 Param <- rollup %>% 
   filter(Assessed_in_2018 == "YES") %>%
   left_join(Pollutant, by = 'Pollu_ID') %>% 
-  left_join(BU_2_Pollu, by = 'Pollu_ID') %>% 
+  left_join(BU_2_Pollu, by = c('Pollu_ID','WQstd_code')) %>% 
   mutate(Para_Attainment = case_when(IR_category == 'Category 2' ~ "meeting criteria",
                                  IR_category %in% c('Category 5','Category 4','Category 4A','Category 4B','Category 4C') ~ "not meeting criteria",
                                  IR_category %in% c('Category 3','Category 3D','Category 3B','Category 3C') ~ "not enough information", 
@@ -35,32 +40,96 @@ Param <- rollup %>%
   mutate(Para_Status = case_when(IR_category == 'Category 2' ~ "Meeting Criteria",
                                      IR_category %in% c('Category 5','Category 4','Category 4A','Category 4B','Category 4B') ~ "Cause",
                                      TRUE ~ "")) %>%
-  mutate(Param_Indicator = ifelse(IR_category == 'Category 4C', "N","Y")) %>%
+  mutate(Param_Indicator = ifelse(IR_category %in% c('Category 2','Category 4C','Category 3','Category 3D',
+                                                   'Category 3B','Category 3C'),"N","Y")) %>%
   mutate(Param_trend = "U",
-         PARAM_COMMENT = Rational,
+         PARAM_COMMENT = Rationale,
          Para_agency_code = "S", 
          Param_Listed_year = Year_listed,
-         PARAM_PRIORITY_RANKING = "Low") 
+         PARAM_PRIORITY_RANKING = "Low") %>% ### change to update based on TMDL table 
+  left_join(delistings, by = c('AU_ID','Pollu_ID','WQstd_code','Period')) %>% 
+  distinct()
   
-### filtered down table for upload 
-Param_upload <- Param %>%
-                select(AU_ID,Attains_PolluName,ben_use,Para_Status,Para_Attainment,Para_agency_code,
-                       Param_Indicator,Param_Listed_year,PARAM_PRIORITY_RANKING,PARAM_COMMENT) %>%
-                rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,
-                       PARAM_STATUS_NAME = Para_Status,PARAM_ATTAINMENT_CODE=Para_Attainment,
-                       PARAM_AGENCY_CODE=Para_agency_code,PARAM_POLLUTANT_INDICATOR=Param_Indicator,
-                       PARAM_YEAR_LISTED=Param_Listed_year)
-write.csv(Param_upload,"Param_102624.csv", row.names = FALSE) 
 
-### build seasonal ####
-seasons <- Param %>% 
-           left_join(LU_spawn, by = "SpawnCode") %>% 
-           select(AU_ID,Attains_PolluName,ben_use,Para_Attainment,SpawnStart,SpawnEnd) %>%
-           rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,PARAM_ATTAINMENT_CODE=Para_Attainment,
-                  SEASON_START=SpawnStart,SEASON_END=SpawnEnd)
+### build seasonal #### 
+# must first run AU_Spawn_dates.R
+Temp_s <- Param %>% 
+  filter(Char_Name == 'Temperature' & Period =='Spawning') 
 
-write.csv(seasons,"Season_102624.csv", row.names = FALSE)            
-  
+Temp_spawn_Start <- Temp_s %>%
+  left_join(AU_Temp_Spawn, by = "AU_ID") %>%
+  filter(!is.na(Temp_SpawnStart)) %>%
+  select(AU_ID, Temp_SpawnStart,start_rank) %>%
+  group_by(AU_ID) %>% 
+  mutate(min_start = ifelse(start_rank == min(start_rank), Temp_SpawnStart, "X")) %>%
+  filter(!min_start == 'X') %>%
+  distinct()
+
+Temp_spawn_End <- Temp_s %>%
+  left_join(AU_Temp_Spawn, by = "AU_ID") %>%
+  filter(!is.na(Temp_SpawnStart)) %>%
+  select(AU_ID,Temp_SpawnEnd,end_rank) %>%
+  group_by(AU_ID) %>% 
+  mutate(max_end = ifelse(end_rank == max(end_rank), Temp_SpawnEnd, "X")) %>%
+  filter(!max_end == 'X') %>%
+  distinct()
+
+Temp_Spawn <- Temp_s %>% 
+  left_join(Temp_spawn_Start, by = 'AU_ID') %>% 
+  left_join(Temp_spawn_End, by = 'AU_ID') %>% 
+  select(AU_ID,Attains_PolluName,ben_use,Para_Attainment,min_start,max_end) %>%
+  rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,PARAM_ATTAINMENT_CODE=Para_Attainment,
+  SEASON_START=min_start,SEASON_END=max_end)
+
+Temp_y <- Param %>% 
+  filter(Char_Name == 'Temperature'& Period =='Year Round') %>% 
+  mutate(min_start = "1/1") %>%
+  mutate(max_end = "12/31") %>%
+  select(AU_ID,Attains_PolluName,ben_use,Para_Attainment,min_start,max_end) %>%
+  rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,
+         PARAM_ATTAINMENT_CODE=Para_Attainment,
+         SEASON_START=min_start,SEASON_END=max_end)
+
+# Same process for DO   
+DO_s <- Param %>% 
+  filter(Char_Name == 'Dissolved Oxygen' & Period =='Spawning') 
+
+DO_spawn_Start <- DO_s %>%
+  left_join(AU_DO_Spawn, by = "AU_ID") %>%
+  filter(!is.na(DO_SpawnStart)) %>%
+  select(AU_ID, DO_SpawnStart,start_rank) %>%
+  group_by(AU_ID) %>% 
+  mutate(min_start = ifelse(start_rank == min(start_rank), DO_SpawnStart, "X")) %>%
+  filter(!min_start == 'X') %>%
+  distinct()
+
+DO_spawn_End <- DO_s %>%
+  left_join(AU_DO_Spawn, by = "AU_ID") %>%
+  filter(!is.na(DO_SpawnStart)) %>%
+  select(AU_ID,DO_SpawnEnd,end_rank) %>%
+  group_by(AU_ID) %>% 
+  mutate(max_end = ifelse(end_rank == max(end_rank), DO_SpawnEnd, "X")) %>%
+  filter(!max_end == 'X') %>%
+  distinct()
+
+DO_Spawn <- DO_s %>% 
+  left_join(DO_spawn_Start, by = 'AU_ID') %>% 
+  left_join(DO_spawn_End, by = 'AU_ID') %>% 
+  select(AU_ID,Attains_PolluName,ben_use,Para_Attainment,min_start,max_end) %>%
+  rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,PARAM_ATTAINMENT_CODE=Para_Attainment,
+         SEASON_START=min_start,SEASON_END=max_end)
+
+DO_y <- Param %>% 
+  filter(Char_Name == 'Dissolved Oxygen'& Period =='Year Round') %>% 
+  mutate(min_start = "1/1") %>%
+  mutate(max_end = "12/31") %>%
+  select(AU_ID,Attains_PolluName,ben_use,Para_Attainment,min_start,max_end) %>%
+  rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,PARAM_ATTAINMENT_CODE=Para_Attainment,
+         SEASON_START=min_start,SEASON_END=max_end)
+Season <- rbind(Temp_Spawn,Temp_y,DO_Spawn,DO_y) %>% distinct()
+
+write.csv(Season,"ATTAINS/ATTAINS_UPLOAD/Season_owyhee_umpqua.csv", row.names = FALSE) 
+
 #### Build ATTAINS Assessment- uses table####
 # should we add additional data - monitoring location information ? 
 Use <- Param %>%
@@ -82,8 +151,23 @@ Use <- Param %>%
   select(AU_ID,ben_use.x,AU_Use_Status,Use_agency_code) %>%
   rename(ASSESSMENT_UNIT_ID = AU_ID,USE_NAME = ben_use.x, USE_ATTAINMENT_CODE = AU_Use_Status, USE_AGENCY_CODE = Use_agency_code)
 
-write.csv(Use,"Use_102624.csv", row.names = FALSE) 
+### filtered down table for upload and checked for designated uses
+Param_upload <- Param %>%
+  left_join(Use, by = c('AU_ID' = 'ASSESSMENT_UNIT_ID','ben_use' = 'USE_NAME')) %>%
+  filter(!is.na(USE_ATTAINMENT_CODE)) %>%
+  select(AU_ID,Attains_PolluName,ben_use,Para_Status,Para_Attainment,Para_agency_code,
+         Param_Indicator,Param_Listed_year,PARAM_PRIORITY_RANKING,PARAM_COMMENT,
+         'Delisting Reason Code',Rationale.y) %>%
+  rename(ASSESSMENT_UNIT_ID = AU_ID,PARAM_NAME=Attains_PolluName,PARAM_USE_NAME=ben_use,
+         PARAM_STATUS_NAME = Para_Status,PARAM_ATTAINMENT_CODE=Para_Attainment,
+         PARAM_AGENCY_CODE=Para_agency_code,PARAM_POLLUTANT_INDICATOR=Param_Indicator,
+         PARAM_YEAR_LISTED=Param_Listed_year,PARAM_DELISTING_REASON ='Delisting Reason Code',
+         PARAM_DELISTING_COMMENT = Rationale.y) %>%
+  mutate(PARAM_DELISTING_AGENCY = "S")
 
+
+write.csv(Use,"ATTAINS/ATTAINS_UPLOAD/Use_owyhee_umpqua.csv", row.names = FALSE) 
+write.csv(Param_upload,"ATTAINS/ATTAINS_UPLOAD/Param_owyhee_umpqua.csv", row.names = FALSE) 
 
 
 #### Build ATTAINS Assessment- general table####
@@ -99,6 +183,6 @@ Gen <- Param %>%
   select(AU_ID,AGENCY_CODE,CYCLE_LAST_ASSESSED) %>% 
   rename(ASSESSMENT_UNIT_ID = AU_ID)
 
-write.csv(Gen,"Gen_102624.csv", row.names = FALSE)
+write.csv(Gen,"ATTAINS/ATTAINS_UPLOAD/Gen_owyhee_umpqua.csv", row.names = FALSE)
 
 
