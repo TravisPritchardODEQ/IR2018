5b9f44f0a462da7ecd4f1ff70afe8e24d528f3e1
Travis Pritchard <TPRITCH@deq.state.or.us>
2020-04-09 10:21:09 -0700

    Actions Rollup
    Tools to combine actio files and create lesley's associated_action file

--- Other tools/Actions_rollup.R
+++ Other tools/Actions_rollup.R
@@ -86,3 +86,79 @@ incorrect_AU_Action_put_together <- AU_Action_put_together %>%
 
 write.xlsx(incorrect_AU_Action_put_together, file = "Incorrect_4a_Assignments_ACTIONS.xlsx")
 
+
+
+
+# Action AU Parameter combine ----------------------------------------------------------
+AU_Actions <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Final List/Misc/older action files/AU_Action.csv",
+                       stringsAsFactors = FALSE)%>%
+  select(AU_ID, Action_ID) %>%
+  mutate(Action_ID = str_replace_all(Action_ID, "[\r\n]" , "")) %>%
+  rename(ACTION_ID = Action_ID)
+
+
+
+Action_Parameter <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Final List/Misc/older action files/Action_Parameter.csv",
+                             stringsAsFactors = FALSE) %>%
+  select(ACTION_ID, Pollu_ID) %>%
+  mutate(Pollu_ID = as.character(Pollu_ID))
+
+con <- DBI::dbConnect(odbc::odbc(), "IR 2018")
+
+
+Pollutants <- DBI::dbReadTable(con, 'LU_Pollutant') %>%
+  mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+  select(-SSMA_TimeStamp) %>%
+  mutate(Pollutant_DEQ.WQS = trimws(Pollutant_DEQ.WQS, which = "right")) %>%
+  select(Pollu_ID,Pollutant_DEQ.WQS )
+
+Action_AU_Parameter <- Action_Parameter %>%
+  left_join(Pollutants,by = "Pollu_ID") %>%
+  right_join(AU_Actions) %>%
+  select(ACTION_ID, AU_ID, Pollu_ID, Pollutant_DEQ.WQS)
+
+write.csv(Action_AU_Parameter, file="//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Final List/Misc/Action_AU_Parameter.csv",
+          row.names = FALSE)
+
+
+# associated_actions ------------------------------------------------------
+
+  AU_Actions <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Final List/Misc/older action files/AU_Action.csv",
+                         stringsAsFactors = FALSE)%>%
+  select(AU_ID, Action_ID) %>%
+  mutate(Action_ID = str_replace_all(Action_ID, "[\r\n]" , "")) %>%
+  rename(ACTION_ID = Action_ID)
+
+
+
+Action_Parameter <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Final List/Misc/older action files/Action_Parameter.csv",
+                             stringsAsFactors = FALSE) %>%
+  select(ACTION_ID, Pollu_ID) %>%
+  mutate(Pollu_ID = as.character(Pollu_ID))
+
+con <- DBI::dbConnect(odbc::odbc(), "IR 2018")
+
+
+Pollutants <- DBI::dbReadTable(con, 'LU_Pollutant') %>%
+  mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+  select(-SSMA_TimeStamp) %>%
+  mutate(Pollutant_DEQ.WQS = trimws(Pollutant_DEQ.WQS, which = "right")) %>%
+  select(Pollu_ID,Attains_PolluName )
+
+
+
+actions <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Final List/Misc/actions.csv",
+                    stringsAsFactors = FALSE) %>%
+  select(ACTION_ID, ACTION_NAME, ACTION_TYPE)
+
+
+associated_actions <- Action_Parameter %>%
+  left_join(Pollutants,by = "Pollu_ID") %>%
+  right_join(AU_Actions) %>%
+  left_join(actions) %>%
+  select(AU_ID, Attains_PolluName,ACTION_ID,  ACTION_TYPE)
+
+write.xlsx(associated_actions, file = "associated_actions.xlsx")
+
+
+
